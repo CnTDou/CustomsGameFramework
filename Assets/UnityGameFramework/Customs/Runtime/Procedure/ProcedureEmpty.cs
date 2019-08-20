d81eadc2d3bd441fa7fb520bf5957058fbfc77ba
GuoJinMing <Jinming@6Sring.com>
2019-08-20 17:28:56 +0800

    add empty procedure

---
+++ Assets/UnityGameFramework/Customs/Runtime/Procedure/ProcedureEmpty.cs
@@ -0,0 +1,21 @@
+﻿using GameFramework.Localization;
+using System;
+using UnityEngine;
+using UnityGameFramework.Runtime;
+using ProcedureOwner = GameFramework.Fsm.IFsm<GameFramework.Procedure.IProcedureManager>;
+
+namespace UnityGameFramework
+{
+    public class ProcedureEmpty : ProcedureBase
+    {
+        public override bool UseNativeDialog
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+
+    }
+}
